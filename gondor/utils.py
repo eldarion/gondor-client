7684df58da7de0414babee905b9458c5bf9ef166
Brian Rosner <brosner@gmail.com>
2011-01-05 11:44:03 -0700

    added local config parsing for client_key for the newer Gondor API

--- gondor/utils.py
+++ gondor/utils.py
@@ -1,6 +1,17 @@
+import os
 import subprocess
 
 
 def check_output(cmd):
     p = subprocess.Popen([cmd], stdout=subprocess.PIPE, shell=True)
     return p.communicate()[0]
+
+
+def find_nearest(directory, search):
+    directory = os.path.abspath(directory)
+    parts = directory.split(os.path.sep)
+    for idx in xrange(len(parts)):
+        d = os.path.sep.join(parts[:-idx])
+        if os.path.isdir(os.path.join(d, search)):
+            return d
+    raise OSError
