c64122046b7f00c4f1a1c5ad329f7eb073c6937e
Brian Rosner <brosner@gmail.com>
2012-09-19 14:59:26 -0600

    Quick test for Windows reverting socket I/O to select

--- gondor/run.py
+++ gondor/run.py
@@ -37,8 +37,6 @@ def win32_run_poll(sock):
     if not confirm("Would you like to run it anyways?"):
         sys.exit(0)
     import win32api, win32console, win32event, win32file
-    sock_event = win32event.CreateEvent(None, True, False, None)
-    win32file.WSAEventSelect(sock.fileno(), sock_event, win32file.FD_CLOSE | win32file.FD_READ)
     import ctypes
     win32 = ctypes.windll.kernel32
     hin = win32.GetStdHandle(-10)
@@ -49,23 +47,18 @@ def win32_run_poll(sock):
     mode = mode & (~0x0002) # disable line input
     mode = mode & (~0x0004) # disable echo input
     win32.SetConsoleMode(hin, mode)
-    stdin = win32api.GetStdHandle(win32api.STD_INPUT_HANDLE)
     handles = [stdin, sock_event]
-    try:
-        while True:
-            i = win32event.WaitForMultipleObjects(handles, 0, 1000)
-            if i == win32event.WAIT_TIMEOUT:
+    while True:
+        try:
+            rr, rw, er = select.select([sock], [], [], 0.1)
+        except select.error, e:
+            if e.args[0] == errno.EINTR:
                 continue
-            if handles[i] == stdin:
-                buf = ctypes.create_string_buffer(1024)
-                bytes_read = ctypes.c_int(0)
-                win32.ReadFile(hin, ctypes.byref(buf), 1024, ctypes.byref(bytes_read), None)
-                sock.send(buf.value)
-            if handles[i] == sock_event:
-                data = sock.recv(4096)
-                if not data:
-                    break
-                sys.stdout.write(data)
-                win32event.ResetEvent(sock_event)
-    finally:
-        win32api.CloseHandle(sock_event)
+            raise
+        if sock in rr:
+            data = sock.recv(4096)
+            if not data:
+                break
+            while data:
+                n = os.write(sys.stdout.fileno(), data)
+                data = data[n:]
